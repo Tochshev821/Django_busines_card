927c0d799da22584296a5d370552178da77d2a83
user <gtoshev2@mail.ru>
2021-08-30 16:38:53 +0300

    Добавил добавление статей в бд. На кнопку домой все ще не нажимается. Стили где стать нужно настроить(test.htm это проблемма нужно устранить)

--- second_django/main/views.py
+++ second_django/main/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'title': 'Главная страница!!',
+        'values': ['some','Hello','123'],
+        'obj': {
+            'car': 'bmw',
+            'age': 21,
+            'hobby': 'wrestling'
+        }
+    }
+    return render(request, 'main/index.html', data)
 def about(request):
     return render(request, 'main/about.html')
